dfd9fa9f868d875b34b2be93ffc7aff2afbe3470
Champanhet, Matt <Matt.Champanhet@krolldiscovery.com>
2022-02-10 06:36:51 -0600

    forgot appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import {InputTextModule} from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
-import {TableModule} from 'primeng/table';
-import {DividerModule} from 'primeng/divider';
-import {CardModule} from 'primeng/card';
-import {ScrollPanelModule} from 'primeng/scrollpanel';
-import {ToggleButtonModule} from 'primeng/togglebutton';
-
-
-
+import { BrowserModule } from '@angular/platform-browser';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+import { DragDropModule } from 'primeng/dragdrop';
+import { InputTextModule } from 'primeng/inputtext';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { TableModule } from 'primeng/table';
+import { ToggleButtonModule } from 'primeng/togglebutton';
 import { AppComponent } from './app.component';
+import { ChipsModule } from 'primeng/chips';
+import { HttpClientModule } from '@angular/common/http';
+import { FirebaseService } from './firebase.service';
+import { environment } from '../environments/environment';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+
 
 @NgModule({
   declarations: [
@@ -24,9 +30,15 @@ import { AppComponent } from './app.component';
     DividerModule,
     CardModule,
     ScrollPanelModule,
-    ToggleButtonModule
+    ToggleButtonModule,
+    DragDropModule,
+    ChipsModule,
+    HttpClientModule/*,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())*/
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
